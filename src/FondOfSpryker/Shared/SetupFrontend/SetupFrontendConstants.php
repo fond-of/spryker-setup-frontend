dd721a7a3689b608c222ca17244d53880ffd718c
Pascal Fischer <pascal.fischer@fondof.de>
2019-03-08 10:28:45 +0100

    Add build mode to store config

--- src/FondOfSpryker/Shared/SetupFrontend/SetupFrontendConstants.php
+++ src/FondOfSpryker/Shared/SetupFrontend/SetupFrontendConstants.php
@@ -7,9 +7,16 @@ use Spryker\Shared\SetupFrontend\SetupFrontendConstants as SprykerSetupFrontendC
 interface SetupFrontendConstants extends SprykerSetupFrontendConstants
 {
     const ARGUMENT_THEME = 'theme';
+
     const OPTION_FORCE = 'force';
+
     const THEME_BUILD_CHOICE = 'No template name given, generate all?';
+
     const THEME_BUILD_CHOICE_NO = 'no';
+
     const THEME_BUILD_CHOICE_YES = 'yes';
+
     const YVES_THEMES = 'yves_themes';
+
+    const FRONTEND_BUILD_MODE = 'FRONTEND_BUILD_MODE';
 }
--- src/FondOfSpryker/Zed/SetupFrontend/SetupFrontendConfig.php
+++ src/FondOfSpryker/Zed/SetupFrontend/SetupFrontendConfig.php
@@ -7,8 +7,39 @@ use Spryker\Zed\SetupFrontend\SetupFrontendConfig as SprykerSetupFrontendConfig;
 
 class SetupFrontendConfig extends SprykerSetupFrontendConfig
 {
+    /**
+     * @return mixed
+     */
     public function getThemes()
     {
         return $this->get(SetupFrontendConstants::YVES_THEMES, false);
+    }
+
+    /**
+     * @return string
+     */
+    public function getYvesBuildCommand(): string
+    {
+        return 'npm run yves' . $this->getBuildMode();
+    }
+
+    /**
+     * @return string
+     */
+    public function getZedBuildCommand(): string
+    {
+        return 'npm run zed' . $this->getBuildMode();
+    }
+
+    /**
+     * @return string|null
+     */
+    protected function getBuildMode(): ?string
+    {
+        $mode = $this->get(SetupFrontendConstants::FRONTEND_BUILD_MODE, '');
+
+        dump(($mode) ? ':' . $mode : null);
+
+        return ($mode) ? ':' . $mode : null;
     }
 }
